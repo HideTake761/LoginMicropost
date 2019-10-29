6a0d061a6ce151be7b6fd8f3477da510eecaacc7
Hide Take <takenakahideto@hotmail.com>
2019-10-29 09:51:58 +0900

    upload to GitHub20191029

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create!(name:  "Admin User",
+             email: "railsadmin@example.com",
+             password:              "rootbeer",
+             password_confirmation: "rootbeer",
+             admin: true)
